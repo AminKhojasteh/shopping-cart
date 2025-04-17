9b03c95179fb7153efe3f1e2a15ebdf218b6bcd0
Amin Khojasteh <aminkhojasteh.ake@gmail.com>
2025-04-17 18:16:53 +0330

    adding backend development. making a server and connectiong to the database

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+// app.use("/api/auth", authRouter);
+
+const start = async function () {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database is connected");
+    app.listen(process.env.PORT, function () {
+      console.log("server is ready");
+    });
+  } catch (error) {
+    console.log("Error connecting to Database: ", error.message);
+    process.exit(1);
+  }
+};
+
+start();
